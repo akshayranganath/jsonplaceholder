63931566ce2a7b81f7a5eb8f94cbfb0f7bb2909b
Akshay Ranganath <akshay.ranganath@cloudinary.com>
2021-09-29 23:06:09 -0700

    updating and simplifying code

--- src/json_placeholder.py
+++ src/json_placeholder.py
@@ -1,5 +1,4 @@
 import requests
-from requests.api import get
 import logging
 import json
 
@@ -9,62 +8,52 @@ logging.basicConfig(
 )
 
 
-def get_url(url):
+def make_request(url, method='get', data=None, headers=None):
     result = {}
     try:
-        resp = requests.get(url)
-        if resp.status_code == 200:
-            result = resp.json()
-            logging.debug(result)
-    except Exception as e:
-        logging.error(e)
-    return result
-
-
-def post_url(url, data, headers=None):
-    result = {}
-    try:
-        resp = requests.post(
-            url,
-            data=data,
-            headers=headers
-        )
-        if resp.status_code == 201:
-            result = resp.json()
+        if method == 'get':
+            resp = requests.get(url)
+            if resp.status_code == 200:
+                result = resp.json()
+                logging.debug(result)
+        elif method == 'post':
+            resp = requests.post(
+                url,
+                data=data,
+                headers=headers
+            )
+            if resp.status_code == 201:
+                result = resp.json()
+            else:
+                logging.error(
+                    f"Unable to create new object: Status {resp.status_code}")
+        elif method == 'put':
+            resp = requests.put(
+                url,
+                data=data,
+                headers=headers
+            )
+            if resp.status_code == 200 or resp.status_code == 204:
+                result = resp.json()
+            logging.debug(resp)
         else:
-            logging.error(
-                f"Unable to create new object: Status {resp.status_code}")
-    except Exception as e:
-        logging.error(e)
-    return result
+            logging.error(f'Unknown Method: {method}')
 
-
-def put_url(url, data, headers=None):
-    result = {}
-    try:
-        resp = requests.put(
-            url,
-            data=data,
-            headers=headers
-        )
-        if resp.status_code == 200 or resp.status_code == 204:
-            result = resp.json()
-        logging.debug(resp)
     except Exception as e:
         logging.error(e)
     return result
 
 
 def get_all_posts():
-    return get_url('https://jsonplaceholder.typicode.com/posts')
+    return make_request('https://jsonplaceholder.typicode.com/posts')
 
 
 def get_single_post(post_id=1):
-    return get_url(f'https://jsonplaceholder.typicode.com/posts/{post_id}')
+    return make_request(f'https://jsonplaceholder.typicode.com/posts/{post_id}')
 
 
 def get_all_posts_by_user():
-    return get_url('https://jsonplaceholder.typicode.com/posts?userId=1')
+    return make_request('https://jsonplaceholder.typicode.com/posts?userId=1')
 
 
 def create_new_post():
@@ -78,16 +67,18 @@ def create_new_post():
     headers = {
         'Content-type': 'application/json; charset=UTF-8'
     }
-    result = post_url(
+    result = make_request(
         'https://jsonplaceholder.typicode.com/posts',
+        method='post',
         # need to do this to ensure the post body has only plain text data
         data=json.dumps(body),
         headers=headers
     )
-    print(result)
+    return result
 
 
 def update_post(post_id=100):
+    result = {}
     url = f'https://jsonplaceholder.typicode.com/posts/{post_id}'
     # first get the post
     item = get_single_post(post_id)
@@ -100,14 +91,20 @@ def update_post(post_id=100):
             'Content-type': 'application/json; charset=UTF-8'
         }
         # update the post
-        resp = put_url(url,
-                       data=json.dumps(item),
-                       headers=headers
-                       )
-        print(resp)
+        resp = make_request(url,
+                            method='put',
+                            data=json.dumps(item),
+                            headers=headers
+                            )
+        logging.info(resp)
     else:
         logging.error(f'No entry found for post: {post_id}')
+    return result
 
 
 if __name__ == "__main__":
+    get_all_posts()
+    get_single_post()
+    get_all_posts_by_user()
+    create_new_post()
     update_post()
